6b587ef13b69c86196be9fe2bfa636edefb43517
Blackkader <ademkefi400@gmail.com>
2025-05-30 11:31:11 +0100

    Added file uploads and product updates

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -5,16 +5,19 @@ namespace App\Controller;
 use App\Form\ProductsForm;
 use App\Entity\MenProducts;
 use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Component\DependencyInjection\Attribute\Autowire;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\String\Slugger\SluggerInterface;
+use Symfony\Component\HttpFoundation\File\Exception\FileException;
 
 #[Route('/admin', name: 'app.admin')]
 final class AdminController extends AbstractController
 {
     #[Route('/updateproduct/{id?}', name: 'update.product')]
-    public function addProduct(MenProducts $product=null,ManagerRegistry $doctrine, Request $request): Response
+    public function addProduct(MenProducts $product=null,ManagerRegistry $doctrine, Request $request, SluggerInterface $slugger,#[Autowire('%kernel.project_dir%/public/assets/uploads/products')] string $brochuresDirectory): Response
     {
         $new=false;
         if (!$product){
@@ -27,6 +30,29 @@ final class AdminController extends AbstractController
         $form->handleRequest($request);
 
         if($form->isSubmitted()){
+            $image = $form->get('image')->getData();
+
+            
+            if ($image) {
+                $originalFilename = pathinfo($image->getClientOriginalName(), PATHINFO_FILENAME);
+                // this is needed to safely include the file name as part of the URL
+                $safeFilename = $slugger->slug($originalFilename);
+                $newFilename = $safeFilename.'-'.uniqid().'.'.$image->guessExtension();
+
+                // Move the file to the directory where brochures are stored
+                try {
+                    $image->move($brochuresDirectory, $newFilename);
+                } catch (FileException $e) {
+                    // ... handle exception if something happens during file upload
+                }
+
+                // updates the 'brochureFilename' property to store the PDF file name
+                // instead of its contents
+                $product->setImage($newFilename);
+            }
+
+
+
             $entityManager = $doctrine->getManager();
             $entityManager->persist($product);
             $entityManager->flush();
@@ -41,7 +67,7 @@ final class AdminController extends AbstractController
 
 
 
-        return $this->render('admin/addproduct.html.twig', [
+        return $this->render('admin/updateproduct.html.twig', [
             'form' => $form->createView()
         ]);
     }
--- src/Form/ProductsForm.php
+++ src/Form/ProductsForm.php
@@ -3,12 +3,14 @@
 namespace App\Form;
 
 
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+//importing file : 
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use App\Entity\MenProducts;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-
+use Symfony\Component\Validator\Constraints\File;
 class ProductsForm extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
@@ -18,11 +20,30 @@ class ProductsForm extends AbstractType
             ->add('description')
             ->add('price')
             ->add('category')
-            ->add('image')
+            ->add('image', FileType::class, [
+                'label' => "The image of the product",
+
+                // unmapped means that this field is not associated to any entity property
+                'mapped' => false,
+
+                // make it optional so you don't have to re-upload the PDF file
+                // every time you edit the Product details
+                'required' => false,
+
+                // unmapped fields can't define their validation using attributes
+                // in the associated entity, so you can use the PHP constraint classes
+                'constraints' => [
+                    new File(
+                        maxSize: '1024k',
+                        extensions: ['jpg','png','gif'],
+                        extensionsMessage: 'Please upload a valid PDF document',
+                    )
+                ],
+            ])
             ->add('stock')
             ->add('ReleaseDate')
             ->add('review')
-            ->add('Ajout', SubmitType::class)
+            ->add('Add-Update', SubmitType::class)
         ;
     }
 
