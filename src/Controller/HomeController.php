3ba0a6a4f704ab70c749053e312757eb68172b87
Ghr4b <graba.hadi@gmail.com>
2025-05-31 21:21:13 +0100

    working on home

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\MenProductsRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -9,8 +10,25 @@ use Symfony\Component\Routing\Attribute\Route;
 final class HomeController extends AbstractController
 {
     #[Route('/home', name: 'app_home')]
-    public function index(): Response
+    public function index(MenProductsRepository $productRepository): Response
     {
-        return $this->render('home/index.html.twig');
+        $categories = ['forhim', 'forher', 'accessories'];
+        $productsbuilder = [];
+
+        foreach ($categories as $category) {
+            $productsbuilder[$category] = $productRepository->createQueryBuilder('p')
+                ->where('p.category = :category')
+                ->setParameter('category', $category)
+                ->orderBy('p.ReleaseDate', 'DESC')
+                ->setMaxResults(4)
+                ->getQuery()
+                ->getResult();
+        }
+        $Products = array_merge(...array_values($productsbuilder));
+       // dd($Products);
+        return $this->render('home/index.html.twig', [
+            'products' => $Products,
+        ]);
     }
+
 }
